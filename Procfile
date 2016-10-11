ac106e5821b6bbbbe2e6c4e376ba9c3de2d74922
vagrant <j3sue@yahoo.com>
2016-10-11 22:52:31 +0000

    push

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: python timeFromUTC.py 
--- timeFromUTC.py
+++ timeFromUTC.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import sys
 app = Flask(__name__)
 from app import views
 from datetime import datetime
@@ -11,7 +12,7 @@ def time():
     return (str((utcTime-datetime(1970,1,1)).total_seconds()) + " seconds have passed since January 1, 1970!")
 
 if __name__ == "__main__":
-    app.run()
+    app.run(port = sys.argv[1])
 
 #References
 #http://stackoverflow.com/questions/15940280/how-to-get-utc-time-in-python
